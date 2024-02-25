39b18eec698d193a7cf7336ee7eabb5f238e8495
Jacob Eldridge <eldridgejacob15@yahoo.com>
2024-02-24 20:39:00 -0600

    added a custom scrollbar and fixed my hamburger menu issues

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const hamMenu = document.querySelector('.ham-menu');
-
+const leftSide = document.querySelector('.name');
 
 hamMenu.addEventListener('click' , () => {
     let navBar = document.querySelector('.header-right');
     navBar.classList.toggle('active');
     hamMenu.classList.toggle('active');
+    leftSide.classList.toggle('active');
 });
 
 function closeMenu ()  {
     let navBar = document.querySelector('.header-right');
     navBar.classList.remove('active');
     hamMenu.classList.remove('active');
-}
+    leftSide.classList.remove('active');
+};
 
-const links = document.querySelectorAll('.header-right .link');
+let links = document.querySelectorAll('.header-right .link');
+let nameLink = document.querySelector('.name');
 
 links.forEach(link => {
     link.addEventListener('click', () => {
         closeMenu(); 
     });
+});
+
+nameLink.addEventListener('click', () => {
+    closeMenu();
 });
